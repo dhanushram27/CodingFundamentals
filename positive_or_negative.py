ec902dd24f73497d3c25b7204713de05af4c63c9
Dhanushram <127968736+dhanushram27@users.noreply.github.com>
2025-02-08 18:16:21 +0530

    Create positive_or_negative.py

---
+++ positive_or_negative.py
@@ -0,0 +1,10 @@
+def postiveornegative(num):
+    if num > 0:
+       return "Positive"
+    elif num < 0:
+       return "Negative"
+    else:
+       return "Zero"
+
+num = 12
+print(postiveornegative(num))
