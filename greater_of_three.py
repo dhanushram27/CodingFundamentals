b98d3d9fb23952dc06b4643b0125d2dd25fe789e
Dhanushram <127968736+dhanushram27@users.noreply.github.com>
2025-02-14 22:43:26 +0530

    Create greater_of_three.py

---
+++ greater_of_three.py
@@ -0,0 +1,7 @@
+num1, num2, num3 = 1,2,3
+if num1>=num2 and num1>=num2:
+    print("num1 is Greater")
+elif num2>=num1 and num2>=num3:
+    print("num2 is Greater")
+else:
+    print("num3 is Greater")
